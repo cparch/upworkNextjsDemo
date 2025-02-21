07d6f44d9ce16c9096150b63a65cfeddde462f3a
Chad Archila <chadparchila@gmail.com>
2025-02-20 23:32:13 -0800

    remove space

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import CopyBox from "./components/copyBox/CopyBox";
 export default function Home() {
   return (
     <div className={styles.page}>
-
       <main className={styles.main}>
         <div>HEADER</div>
         <HeroImage/>
