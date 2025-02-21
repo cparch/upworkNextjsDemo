f1dc8ef802db3145c44f87ad56580e7d95228b84
Chad Archila <chadparchila@gmail.com>
2025-02-20 23:23:38 -0800

    Create CopyBox component

---
+++ app/components/copyBox/CopyBox.tsx
@@ -0,0 +1,11 @@
+// import { useState, useEffect } from "react";
+import styles from "./copyBox.module.css";
+
+export default function CopyBox() {
+  return (
+    <div div className={styles.mainContainer}>
+      Non praesent elementum viverra mollis sodales. Ut libero rhoncus himenaeos.
+    </div>  
+  );
+}
+
