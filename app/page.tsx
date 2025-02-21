ad41022ebeff53eb3dad4a9e5d055d79fe686935
Chad Archila <chadparchila@gmail.com>
2025-02-20 23:27:55 -0800

    First homePage commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,43 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import  HeroImage  from "./components/heroImage/HeroImage" 
+import ActionImage from "./components/actionImage/ActionImage"
+import CopyBox from "./components/copyBox/CopyBox";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now CPA
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <main className={styles.main}>
+        <div>HEADER</div>
+        <HeroImage/>
+
+        {/*
+          Todo:
+          - Put some big bold white text here at the bottom of the hero image
+          - I want it bottom aligned so you don't see it at first, but as you scroll down it comes into view
+          */}
+
+
+        <div className={styles.imgContainer}>
+          <ActionImage src='https://images.unsplash.com/photo-1616279967983-ec413476e824?q=80&w=2052&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+
+          <CopyBox/>
+
+          {/*
+          Todo:
+          - Center the image and the text to itself in the ActionImage component
+
+          - combine actionImage and copyBox into a single component
+          - make it so that the image and text can be passed in as props
+          - make it so that the text can be positioned to the left or right of the image via props
+          - these components will need to be stacked vertically
+          
+          */}
+    
+ 
         </div>
+
       </main>
       <footer className={styles.footer}>
         <a
