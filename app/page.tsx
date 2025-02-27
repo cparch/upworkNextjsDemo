f57457ecb9b360e6569a571482a3cce2f07b44f7
Chad Archila <chadparchila@gmail.com>
2025-02-26 23:10:52 -0800

    Add new section to hp and remove CRUFT

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import  HeroImage  from "./components/heroImage/HeroImage" 
-import ActionImage from "./components/actionImage/ActionImage"
-import CopyBox from "./components/copyBox/CopyBox";
 // import { Roboto } from '@next/font/google'
 import MyButton from "./components/button/Button";
 import BundleIcon from "./components/iconbundle/IconBundle";
+import InfoSection from "./components/infoSection/InfoSection";
 
 //Tomorrow get finish the:
-// - Icon section
-// - ActionImage component
-// const roboto = Roboto({
-//   subsets: ['latin'],
-//   weight: ['900']
-// })
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <div className={styles.sectionOne}>
-          {/* <div className={`${roboto.className} ${styles.headerText}`}>Vestibulum at nibh at nulla faucibus dapibus</div> */}
-          <HeroImage/>  
+        <div className={styles.sectionOne}>          <HeroImage/>  
           <div className={styles.textContainer}>
             <h1 className={styles.headerText}>Vestibulum at nibh at nulla</h1>
             <p className={styles.subHeaderText}>Vivamus purus mi, mollis nec elit vel, scelerisque rhoncus dolor.</p>
@@ -30,33 +21,7 @@ export default function Home() {
         <div className={styles.sectionTwo}>
           <BundleIcon/>
         </div>
-
-        {/*
-          Todo:
-          - Put some big bold white text here at the bottom of the hero image
-          - I want it bottom aligned so you don't see it at first, but as you scroll down it comes into view
-          */}
-
-
-        <div className={styles.imgContainer}>
-          <ActionImage src='https://images.unsplash.com/photo-1616279967983-ec413476e824?q=80&w=2052&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
-
-          <CopyBox/>
-
-          {/*
-          Todo:
-          - Center the image and the text to itself in the ActionImage component
-
-          - combine actionImage and copyBox into a single component
-          - make it so that the image and text can be passed in as props
-          - make it so that the text can be positioned to the left or right of the image via props
-          - these components will need to be stacked vertically
-          
-          */}
-    
- 
-        </div>
-
+        <InfoSection/>
       </main>
       <footer className={styles.footer}>
         <a
